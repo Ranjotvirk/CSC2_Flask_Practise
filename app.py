85ac5d1c6b295b42d606054c6abcc31c73cc59ed
Ranjotvirk <st25355@ormiston.school.nz>
2026-05-07 12:20:40 +1200

    Done app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/checkout")
+def checkout():
+    return render_template('invioce.html')
+
+@app.route("/orders")
+def order_history():
+    return render_template('order_history.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
